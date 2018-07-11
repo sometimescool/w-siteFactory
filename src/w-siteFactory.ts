da090b2de1319e33df81e2c9ac24aeba4d30dddf
sometimescool <laurent@varciat.com>
2018-07-11 09:05:19 +0200

    Publication de quelques méthode du helper

--- src/w-siteFactory.ts
+++ src/w-siteFactory.ts
@@ -7,6 +7,8 @@ import * as ImgZoom from "./imageZoom/controller";
 import * as List from "./listes/controller";
 import * as ResponsiveTopNav from "./responsiveTopnav/controller";
 import * as ImageDetail from "./imageDetail/controller";
+import * as helper from "./helpers/helper";
+
 export function calendarFactory(): void {
     Calendar.calendarFactory()
 }
@@ -47,4 +49,25 @@ export function responsiveTopNav(key: string, size: number | undefined) {
 }
 export function imageDetailFactory(maxWidth: number | null): void {
     ImageDetail.imageDetailFactory(maxWidth);
+}
+export function onOrientationChange(callBack:Function):string{
+    return helper.orientation.onOrientationChange(callBack);
+}
+export function removeOrientationChange(key:string):void{
+    helper.orientation.removeOrientationChange(key);
+}
+export function UUID():string {
+    return helper.UUID();
+}
+export function clickHandler(element:HTMLElement):void{
+    helper.CDom.clickHandler(element)​
+}
+export function mouseMoveHandler(element:HTMLElement):void{
+    helper.CDom.mouseMoveHandler(element)​
+}
+export function dataClick(element:HTMLElement):string{
+    return  helper.CDom.dataClick(element);
+}
+​export function dataMouseMove(element:HTMLElement):string{
+    return  helper.CDom.dataMouseMove(element);
 }
